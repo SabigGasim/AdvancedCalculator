31ae8d6edfeba6a20f1a726d99eb3bae949a4e45
سابق بكري <sabgasim@gmail.com>
2025-10-01 01:40:07 +0300

    ensure parentheses are balanced inside the regex instead of code

--- src/MathSyntaxValidator.cs
+++ src/MathSyntaxValidator.cs
@@ -5,36 +5,13 @@ namespace AdvancedCalculator;
 
 public partial class MathSyntaxValidator
 {
-    [GeneratedRegex(@"^-?(?:\d+(?:\.\d+)?|(?:(?:a?(?:sin|cos|tan|csc|sec|cot|sqrt|cbrt)))?\(\s*[^+\/*^\s].*?\s*\))(?:[+\-/*^](?:\d+(?:\.\d+)?|(?:(?:a?(?:sin|cos|tan|csc|sec|cot|sqrt|cbrt)))?\(\s*[^+\/*^\s].*?\s*\)))*$")]
+    [GeneratedRegex(@"^-?(?:\d+(?:\.\d+)?|(?:(?:a?(?:sin|cos|tan|csc|sec|cot|sqrt|cbrt)))?(?<p>\((?!\s*\))(?!\s*[+/*^])(?:[^()]|(?<p>\()|(?<-p>\)))*)(?<-p>\)))(?:[+\-/*^](?:\d+(?:\.\d+)?|(?:(?:a?(?:sin|cos|tan|csc|sec|cot|sqrt|cbrt)))?(?<p>\((?!\s*\))(?!\s*[+/*^])(?:[^()]|(?<p>\()|(?<-p>\)))*)(?<-p>\))))*(?(p)(?!))$")]
     private static partial Regex SimpleMathExpression();
 
     public static bool IsSimpleExpression([NotNullWhen(true)] string? input)
     {
         return SimpleMathExpression().IsMatch(input ?? "")
-            && AllOpenParenthesesAreBalanced(input!)
             && AllNumbersAreValidDoubles(input!);
-    }
-
-    private static bool AllOpenParenthesesAreBalanced(string input)
-    {
-        int openParanthesesNumber = 0;
-
-        for (int i = 0; i < input!.Length; i++)
-        {
-            openParanthesesNumber += input[i] switch
-            {
-                '(' => 1,
-                ')' => -1,
-                _ => 0
-            };
-
-            if (openParanthesesNumber < 0)
-            {
-                return false;
-            }
-        }
-
-        return openParanthesesNumber == 0;
     }
 
     private static bool AllNumbersAreValidDoubles(string input)
