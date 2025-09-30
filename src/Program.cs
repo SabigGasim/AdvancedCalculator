eacf6841db1815a88b6390bb6591a710a4d6d321
سابق بكري <sabgasim@gmail.com>
2025-09-30 22:45:26 +0300

    add simple math syntax validation

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdvancedCalculator;
+
+do
+{
+    Console.Write("Write expression: ");
+    var expression = Console.ReadLine();
+    var result = Calculator.Calculate(expression);
+    if (!result.Success)
+    {
+        Console.WriteLine($"An error occurred: {result.ErrorMessage}");
+        continue;
+    }
+
+    Console.WriteLine($"Result is: {result.Payload:g}");
+}
+while (ShouldRestart());
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+static bool ShouldRestart()
+{
+    Console.Write("Want to try again? (Y/N): ");
+    return Console.ReadLine() is ("y" or "Y");
+}
---
+++ src/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace AdvancedCalculator;
+
+public class Result<T>
+{
+    private Result() { }
+
+    public T Payload { get; init; } = default!;
+    public bool Success { get; init;  }
+    public string? ErrorMessage { get; init; }
+
+    public static Result<T> CreateSuccess(T payload)
+    {
+        return new Result<T>
+        {
+            Payload = payload,
+            Success = true,
+        };
+    }
+
+    public static Result<T> CreateFailure(string? errorMessage)
+    {
+        return new Result<T>
+        {
+            ErrorMessage = errorMessage,
+            Success = false,
+        };
+    }
+
+    public static implicit operator Result<T>(T payload) => Result<T>.CreateSuccess(payload); 
+}
